5dbbadd3070db9177a90e419b6fa740450f75553
Kieran Healy <kjhealy@gmail.com>
2015-12-15 10:16:27 -0500

    revenue/employee plot

--- yahoo.r
+++ yahoo.r
@@ -6,6 +6,8 @@ theme_set(theme_minimal())
 
 data <- read.csv("data/data.csv", header = TRUE)
 
+
+## Scatterplot
 p <- ggplot(data, aes(x = Employees, y = Revenue))
 
 p + geom_smooth(method = "lm") +
@@ -21,7 +23,7 @@ credit("\nNote: Two observations for 2012 (Mayer appointed in July). Kieran Heal
 
 
 
-
+## Line segements with year labels
 p + geom_path(color = "gray80") +
     geom_text(aes(color = Mayer, label = Year), size = 3, fontface = "bold") +
     theme(legend.position = "bottom") +
@@ -31,4 +33,18 @@ p + geom_path(color = "gray80") +
     ggtitle("Yahoo Employees vs Revenues, 2004-2014") +
     scale_y_continuous(labels = dollar) +
     scale_x_continuous(labels = comma)
-credit("\nNote: Two observations for 2012 (Mayer appointed in July). Kieran Healy, http://kieranhealy.org ")
+credit("\nNote: Two observations for 2012 (Mayer appointed in July). Kieran Healy, http://kieranhealy.org")
+
+
+### Revenue/Employee ratio over time
+p <- ggplot(data,
+            aes(x = Year, y = Revenue/Employees))
+
+p + geom_vline(xintercept = 2012) +
+    geom_line(color = "gray60", size = 2) +
+    annotate("text", x = 2013, y = 0.44, label = " Mayer becomes CEO", size = 2.5) +
+    labs(x = "Year\n",
+         y = "Revenue/Employees") +
+    ggtitle("Yahoo Revenue to Employee Ratio, 2004-2014")
+
+credit("\n\nNote: Two observations for 2012 (Mayer appointed in July). Kieran Healy, http://kieranhealy.org")
